11e5f2321df9693450c3d82a351275a9d7a7cd4e
teric622 <tlasecaeric@gmail.com>
2023-10-03 07:14:59 -0400

    new changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 (function() {
     // https://dashboard.emailjs.com/admin/account
-    emailjs.init('EQ3S1LJ6FsKDk6vJm');
+    emailjs.init('service_azzxp96');
 })();
 
 
@@ -9,7 +9,7 @@ window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
         // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
+        // this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
         emailjs.sendForm('contact_service', 'contact_form', this)
             .then(function() {
